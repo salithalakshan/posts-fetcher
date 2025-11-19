141a2225d9432d4a3f141381b1b803ec9758b500
salithalakshan <salithalakshan@gmail.com>
2025-11-19 19:42:04 +0530

    Add project files.

---
+++ CreateDb.sql
@@ -0,0 +1,20 @@
+
+BEGIN
+    CREATE DATABASE ExternalCacheService;
+END
+
+
+
+BEGIN
+    CREATE TABLE dbo.CacheEntry
+    (
+        Id            INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        [Key]         NVARCHAR(200)     NOT NULL UNIQUE,
+        JsonValue     NVARCHAR(MAX)     NOT NULL,
+        CreatedAtUtc  DATETIME2(3)      NOT NULL,
+        ExpiresAtUtc  DATETIME2(3)      NOT NULL
+    );
+
+	CREATE NONCLUSTERED INDEX IX_CacheEntry_ExpiresAtUtc
+        ON dbo.CacheEntry (ExpiresAtUtc);
+END
